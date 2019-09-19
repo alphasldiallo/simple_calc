0056edc5b92551f431ad547d37bbd1a1cdfec38e
Alpha Mamadou Diallo <alphadiallo009@gmail.com>
2019-09-19 23:41:09 +0200

    Revert "Revert "UI interfaces""
    This reverts commit 4e87ef76d68ee8a724f48b5b9a4c4b6bb7c819ad.

---
+++ src/intro_software_arch/Intro_software_arch.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intro_software_arch;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+/**
+ *
+ * @author Admin
+ */
+public class Intro_software_arch {
+
+    /**
+     * @param args the command line arguments
+     */
+        static int n1, n2 = 0;
+        
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+        
+        JFrame fen = new JFrame();
+        fen.setTitle("First window");
+        fen.setSize(400, 100);
+        fen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel pan = new JPanel();
+        pan.setBackground(Color.ORANGE);
+        JLabel title = new JLabel("My simple calculator");
+        JLabel result = new JLabel();
+        JButton b = new JButton("Result");
+        JTextField num1 = new JTextField("", 4);
+        num1.setBounds(300, 70, 200, 30);
+        JTextField num2 = new JTextField("", 4);
+        b.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent event){
+                n1 = Integer.parseInt(num1.getText());
+                n2 = Integer.parseInt(num2.getText());
+                int rsl = n1+n2;
+                result.setText(Integer.toString(rsl));
+            }
+        });
+        JLabel sign = new JLabel("+");
+        JLabel eq = new JLabel("=");
+        pan.add(title);
+        pan.add(num1);
+        pan.add(sign);
+        pan.add(num2);
+        pan.add(eq);
+        pan.add(result);
+        
+        
+        pan.add(b);
+        fen.setContentPane(pan);
+        
+        fen.setVisible(true);
+    }
+    
+}
